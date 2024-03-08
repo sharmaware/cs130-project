46c42ce9f54f1804bfc7ea4a87f598753afdc4a6
Saarthak Sharma <sharma.saarthak@gmail.com>
2024-03-08 01:55:36 -0800

    changing port to 8080

--- aws/scripts/application-stop.sh
+++ aws/scripts/application-stop.sh
@@ -2,7 +2,11 @@
 set -x
 
 # System control will return either "active" or "inactive".
-tomcat_running=$(systemctl is-active tomcat)
-if [ "$tomcat_running" == "active" ]; then
-    service tomcat stop
+# Check if port 8080 is being listened to
+if lsof -Pi :8080 -sTCP:LISTEN -t >/dev/null; then
+    # If a program is listening on port 8080, kill it
+    lsof -Pi :8080 -sTCP:LISTEN -t | xargs kill -9
+    echo "Program listening on port 8080 has been killed."
+else
+    echo "No program is listening on port 8080."
 fi
