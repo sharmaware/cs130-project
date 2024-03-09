608aa157b262560988219a3f3c271a89d3b34ac0
Saarthak Sharma <sharma.saarthak@gmail.com>
2024-03-08 17:37:17 -0800

    running on port 8080 and updating application stop

--- aws/scripts/application-stop.sh
+++ aws/scripts/application-stop.sh
@@ -3,9 +3,9 @@ set -x
 
 # System control will return either "active" or "inactive".
 # Check if port 8080 is being listened to
-if lsof -Pi :8080 -sTCP:LISTEN -t >/dev/null; then
+if sudo lsof -Pi :8080 -sTCP:LISTEN -t >/dev/null; then
     # If a program is listening on port 8080, kill it
-    lsof -Pi :8080 -sTCP:LISTEN -t | xargs kill -9
+    sudo lsof -Pi :8080 -sTCP:LISTEN -t | xargs kill -9
     echo "Program listening on port 8080 has been killed."
 else
     echo "No program is listening on port 8080."
